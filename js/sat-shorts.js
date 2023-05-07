d95c4b3468a0052bae72eb0269a9abeaf02847ed
Aaron <mail@aosterwyk.com>
2023-05-07 14:14:38 -0500

    added utc timestmap

--- js/sat-shorts.js
+++ js/sat-shorts.js
@@ -1,7 +1,26 @@
-
+// timestamp timer
 setInterval(() => {
-    let timestamp = document.getElementById('UTCtime');
-    const date = new Date();
-    let newTimeString = `${date.toUTCString()}`;
-    timestamp.innerText = newTimeString;
+    const date = new Date();   
+    let currentHour = ('0' + date.getUTCHours()).slice(-2);
+    let currentMin = ('0' + date.getUTCMinutes()).slice(-2);
+    let currentSec = ('0' + date.getUTCSeconds()).slice(-2);
+    const formattedTime = date.toLocaleTimeString("en-US", {
+        timeZone: "UTC",
+        hour12: false,
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    });
+    let timestamp = document.getElementById('UTCTime');
+    timestamp.innerText = formattedTime;
+
+    const formattedDate = date.toLocaleDateString("en-US", {
+        timeZone: "UTC",
+        year: "numeric",
+        month: "long",
+        day: "2-digit"
+    });
+    let datestamp = document.getElementById('UTCDate');
+    datestamp.innerText = `${formattedDate} (UTC)`;    
 },1000);
+
